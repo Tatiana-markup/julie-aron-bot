2185c49746c5449868c9b8c580f8b97bf99aff1f
TANYA <Tanya@MacBook-Pro.local>
2025-10-01 23:51:43 +0300

    Виправлено user.js: багатомовні повідомлення, SEPA реквізити, авто-зменшення stock

--- index.js
+++ index.js
@@ -1,23 +1,25 @@
 require('dotenv').config();
 const express = require('express');
 
-const { userBot, orders, stock } = require('./user');
-const { adminBot } = require('./admin');
+const { userBot, orders, getStock, setStock } = require('./user');
+const admin = require('./admin'); // тут ми забираємо і bot, і init
 
 const app = express();
 app.use(express.json());
 
-// --- Webhooks ---
+// ініціалізація адмін-бота
+admin.init({ orders, getStock, setStock });
+const adminBot = admin.bot;
+
+// підключаємо вебхуки
 app.use(userBot.webhookCallback('/user'));
 app.use(adminBot.webhookCallback('/admin'));
 
 userBot.telegram.setWebhook(process.env.WEBHOOK_URL + '/user');
 adminBot.telegram.setWebhook(process.env.WEBHOOK_URL + '/admin');
 
-// --- Root route ---
 app.get('/', (req, res) => res.send('Julie & Aron Bot работает 🚀'));
 
-// --- Start server ---
 app.listen(process.env.PORT || 8080, () => {
   console.log('Server running on port', process.env.PORT || 8080);
 });
